c7912ede835d4a8912dd21e9b48a93c5629c854e
futuretea <1913508671@qq.com>
2021-01-21 10:31:42 +0800

    update

--- k3s-restarter-installer.sh
+++ k3s-restarter-installer.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
 
-sudo tee /opt/k3s-restarter >/dev/null <<EOF
+BIN_DIR=${INSTALL_K3S_RESTARTER_BIN_DIR:-"/sbin"}
+
+sudo tee ${BIN_DIR}/k3s-restarter >/dev/null <<EOF
 #!/usr/bin/env bash
 
 echo "\$\$" >/var/run/k3s-restarter-trap.pid
@@ -11,14 +14,14 @@ handler(){
 trap handler SIGHUP
 tail -f /dev/null & wait \$!
 EOF
-sudo chmod +x /opt/k3s-restarter
+sudo chmod +x ${BIN_DIR}/k3s-restarter
 
-sudo tee /etc/init.d/k3s-restarter.service >/dev/null <<EOF
+sudo tee /etc/init.d/k3s-restarter-service >/dev/null <<EOF
 #!/sbin/openrc-run
 
 supervisor=supervise-daemon
-name="k3s-restarter"
-command="/opt/k3s-restarter"
+name="k3s-restarter-service"
+command="${BIN_DIR}/k3s-restarter"
 command_args=">/var/log/k3s-restarter.log 2>&1"
 
 output_log=/var/log/k3s-restarter.log
@@ -28,7 +31,12 @@ pidfile="/var/run/k3s-restarter.pid"
 respawn_delay=5
 respawn_max=0
 EOF
-sudo chmod +x /etc/init.d/k3s-restarter.service
+sudo chmod +x /etc/init.d/k3s-restarter-service
 
-sudo rc-update add k3s-restarter.service
-sudo rc-service k3s-restarter.service restart
+if [ "${INSTALL_K3S_RESTARTER_SKIP_ENABLE}" != true ]; then
+  sudo rc-update add k3s-restarter-service
+fi
+
+if [ "${INSTALL_K3S_RESTARTER_SKIP_START}" != true ]; then
+  sudo rc-service k3s-restarter-service restart
+fi
